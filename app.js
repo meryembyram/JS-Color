13ab088fe0f67f92d317f7ec9edb2c27fa630c94
Meryem Bayram <99353586+meryembyram@users.noreply.github.com>
2022-05-26 19:14:47 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,14 @@
+const button = document.querySelector(".btn");
+const color = document.querySelector(".color");
+const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+button.addEventListener("click", function (){
+    let hexColor = "#";
+    for (let i=0; i<6 ; i++){
+        hexColor += hex[getRandomNumber()];
+    }
+    color.textContent = hexColor;
+    document.body.style.backgroundColor = hexColor;
+});
+  function getRandomNumber(){
+      return Math.floor(Math.random() * hex.length);
+  };
